a4b27943e5a4bf68cd6016cb8dc34fc2f8467304
chenxionglin <43723206+chenxionglin@users.noreply.github.com>
2022-05-04 07:55:30 +0800

    修改运行所需配置文件

--- CrawlNodes/pipelines.py
+++ CrawlNodes/pipelines.py
@@ -16,5 +16,5 @@ class CrawlnodesPipeline:
         if isinstance(item, ClashItem):
            # print(item)
             pd.DataFrame(item['nodes']).to_csv(
-                'CrawlNodes/datas/node/nodes.csv', mode='a', header=False, index=False)
+                'datas/node/nodes.csv', mode='a', header=False, index=False)
         return item
