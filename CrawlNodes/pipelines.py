e503cf9c8a04f4ccd2216e6e987368ee9ac59404
chenxionglin <43723206+chenxionglin@users.noreply.github.com>
2022-05-04 06:39:57 +0800

    ''

---
+++ CrawlNodes/pipelines.py
@@ -0,0 +1,20 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import pandas as pd
+from CrawlNodes.items import ClashItem
+
+
+class CrawlnodesPipeline:
+
+    def process_item(self, item, spider):
+        if isinstance(item, ClashItem):
+            print(item)
+            pd.DataFrame(item['nodes']).to_csv(
+                'CrawlNodes/datas/node/nodes.csv', mode='a', header=False, index=False)
+        return item
---
+++ CrawlNodes/spiders/clash_v2ray.py
@@ -0,0 +1,29 @@
+
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from CrawlNodes.items import ClashItem
+
+
+class ClashV2raySpider(CrawlSpider):
+    name = 'clash_v2ray'
+    allowed_domains = ['www.cfmem.com']
+    # redis_key = 'clash_v2ray'
+    start_urls = ['https://www.cfmem.com/search/label/free']
+    # //*[@id="Blog1"]/div/article/div[1]/h2/a
+    rules = (
+        Rule(LinkExtractor(
+            restrict_xpaths=('//*[@id="Blog1"]/div/article/div[1]/h2/a')), callback='parse_item', follow=True),
+        Rule(LinkExtractor(
+            restrict_xpaths=('//*[@id="starter-pro-load-more-link"]')), callback='parse_item', follow=True),)
+
+    def parse_item(self, response):
+        item = ClashItem()
+        title = response.xpath(
+            '//*[@id="Blog1"]/div/article/div/div[1]/h1/text()').get()
+        nodes = response.xpath('//*[@id="post-body"]/div[4]/pre/span')
+        node_list = []
+        for node in nodes:
+            node_list.append(node.xpath('string(.)').get())
+        item['title'] = title
+        item['nodes'] = node_list
+        yield item
