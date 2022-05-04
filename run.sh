ce4cd06149600763877d9851607b82a4bf2141f7
chenxionglin <43723206+chenxionglin@users.noreply.github.com>
2022-05-04 15:52:47 +0800

    爬取v2ray节点

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-cd CrawlNodes/datas/node/ || exit
+cd CrawlNodes/datas/node || exit
 rm -r nodes.csv,nodes.txt
 touch nodes.csv
 scrapy crawl clash_v2ray
-cd datas/node/ && python tobase64.py
+python tobase64.py
 git add .
 git commit -am "爬取v2ray节点"
-git push
+git push -f
