2c45e4b7ccb43feea71a6f6e21061ea09587910f
chenxionglin <43723206+chenxionglin@users.noreply.github.com>
2022-05-04 14:49:00 +0800

    修改run.sh内容

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 cd CrawlNodes/datas/node/ && rm -r nodes.csv
+
+touch nodes.csv
 cd ../../.
 scrapy crawl clash_v2ray
 git add .
-#git commit -m "爬取v2ray节点"
-#git push
+git commit -m "爬取v2ray节点"
+git push
