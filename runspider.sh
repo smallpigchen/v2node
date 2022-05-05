a722da4f747ed1c2bd201e8ca83e07a1cb001497
chenxionglin <43723206+chenxionglin@users.noreply.github.com>
2022-05-05 10:17:20 +0800

    爬取v2ray节点

--- runspider.sh
+++ runspider.sh
@@ -1,8 +1,6 @@
-cd CrawlNodes/datas/node && rm -r nodes.csv,nodes.txt
-touch nodes.csv
-cd ../../..
 scrapy crawl clash_v2ray
 cd CrawlNodes/datas/node && python tobase64.py
+cd ../../../
 git add .
 git commit -am "爬取v2ray节点"
 git push -f
